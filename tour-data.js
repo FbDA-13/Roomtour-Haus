2431d41e6c766901d26bce67b1387e35fc7cb19b
FbDA-13 <frames.ambrusch@gmail.com>
2026-08-01 20:56:51 +0200

    R00

---
+++ tour-data.js
@@ -0,0 +1,488 @@
+window.TOUR_DATA={
+  "homepage": "https://www.framesbyambrusch.at",
+  "scenes": [
+    {
+      "id": "0-aw-13-30-34",
+      "name": "Wohnbereich",
+      "desktop": "images/desktop/scene-01.jpg",
+      "mobile": "images/mobile/scene-01.jpg",
+      "thumbnail": "thumbnails/scene-01.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0731279585588105,
+          "pitch": -0.04590935194940826,
+          "rotation": 0,
+          "target": "2-aw-13-32-34"
+        },
+        {
+          "yaw": 2.886266967938142,
+          "pitch": 0.10545509279351606,
+          "rotation": 0,
+          "target": "1-aw-13-31-08"
+        },
+        {
+          "yaw": -1.3673966714791739,
+          "pitch": 0.042099417731920497,
+          "rotation": 0,
+          "target": "4-aw-13-41-25"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-aw-13-31-08",
+      "name": "Wohnzimmer",
+      "desktop": "images/desktop/scene-02.jpg",
+      "mobile": "images/mobile/scene-02.jpg",
+      "thumbnail": "thumbnails/scene-02.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9522338178489527,
+          "pitch": 0.08555452434854871,
+          "rotation": 0,
+          "target": "14-aw-14-00-30"
+        },
+        {
+          "yaw": -0.16534976729291806,
+          "pitch": 0.07831517682467748,
+          "rotation": 0,
+          "target": "0-aw-13-30-34"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aw-13-32-34",
+      "name": "Essbereich & Küche",
+      "desktop": "images/desktop/scene-03.jpg",
+      "mobile": "images/mobile/scene-03.jpg",
+      "thumbnail": "thumbnails/scene-03.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.368577697320876,
+          "pitch": 0.1260406495860309,
+          "rotation": 0,
+          "target": "0-aw-13-30-34"
+        },
+        {
+          "yaw": -0.2784034944565992,
+          "pitch": -0.021770730612534095,
+          "rotation": 0,
+          "target": "16-aw-14-23-172"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-aw-13-40-53",
+      "name": "Eingangsbereich",
+      "desktop": "images/desktop/scene-04.jpg",
+      "mobile": "images/mobile/scene-04.jpg",
+      "thumbnail": "thumbnails/scene-04.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10044793330668078,
+          "pitch": 0.1258515596444134,
+          "rotation": 0,
+          "target": "4-aw-13-41-25"
+        },
+        {
+          "yaw": -3.0721763862179614,
+          "pitch": 0.014987381873034167,
+          "rotation": 0,
+          "target": "15-aw-14-25-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-aw-13-41-25",
+      "name": "Treppenhaus Erdgeschoss",
+      "desktop": "images/desktop/scene-05.jpg",
+      "mobile": "images/mobile/scene-05.jpg",
+      "thumbnail": "thumbnails/scene-05.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0163200077512347,
+          "pitch": 0.12366563088199811,
+          "rotation": 0,
+          "target": "3-aw-13-40-53"
+        },
+        {
+          "yaw": 2.34355907044461,
+          "pitch": 0.04426168254294183,
+          "rotation": 0,
+          "target": "5-aw-13-42-18"
+        },
+        {
+          "yaw": 1.352940775475778,
+          "pitch": 0.16645048796311812,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        },
+        {
+          "yaw": -2.10655286675817,
+          "pitch": 0.060438082044862895,
+          "rotation": 0,
+          "target": "0-aw-13-30-34"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-aw-13-42-18",
+      "name": "Gäste-WC",
+      "desktop": "images/desktop/scene-06.jpg",
+      "mobile": "images/mobile/scene-06.jpg",
+      "thumbnail": "thumbnails/scene-06.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.012791549883019,
+          "pitch": 0.08761339261767453,
+          "rotation": 0,
+          "target": "4-aw-13-41-25"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-aw-13-43-51",
+      "name": "Galerie Obergeschoss",
+      "desktop": "images/desktop/scene-07.jpg",
+      "mobile": "images/mobile/scene-07.jpg",
+      "thumbnail": "thumbnails/scene-07.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8783252717277374,
+          "pitch": 0.32114444263778275,
+          "rotation": 0,
+          "target": "4-aw-13-41-25"
+        },
+        {
+          "yaw": 1.1346873810108846,
+          "pitch": 0.08057296819023563,
+          "rotation": 0,
+          "target": "8-aw-13-48-53"
+        },
+        {
+          "yaw": 0.3822552066151079,
+          "pitch": 0.09713061852512261,
+          "rotation": 0,
+          "target": "7-aw-13-46-34"
+        },
+        {
+          "yaw": -1.3438325874811028,
+          "pitch": 0.09316528131334856,
+          "rotation": 0,
+          "target": "11-aw-13-53-45"
+        },
+        {
+          "yaw": -1.8119804279041603,
+          "pitch": 0.08361901183502951,
+          "rotation": 0,
+          "target": "10-aw-13-51-17"
+        },
+        {
+          "yaw": -2.340694079135634,
+          "pitch": 0.06521637464001628,
+          "rotation": 0,
+          "target": "9-aw-13-50-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-aw-13-46-34",
+      "name": "Schlafzimmer Master",
+      "desktop": "images/desktop/scene-08.jpg",
+      "mobile": "images/mobile/scene-08.jpg",
+      "thumbnail": "thumbnails/scene-08.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3934344649886707,
+          "pitch": 0.07203329425312788,
+          "rotation": 0,
+          "target": "13-aw-13-55-59"
+        },
+        {
+          "yaw": 2.097199741877331,
+          "pitch": 0.086580097566884,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-aw-13-48-53",
+      "name": "Schlafzimmer",
+      "desktop": "images/desktop/scene-09.jpg",
+      "mobile": "images/mobile/scene-09.jpg",
+      "thumbnail": "thumbnails/scene-09.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1521149981761276,
+          "pitch": -0.027090980633555972,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-aw-13-50-51",
+      "name": "Badezimmer",
+      "desktop": "images/desktop/scene-10.jpg",
+      "mobile": "images/mobile/scene-10.jpg",
+      "thumbnail": "thumbnails/scene-10.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3762543977913175,
+          "pitch": 0.04065170381476335,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-aw-13-51-17",
+      "name": "WC Obergeschoss",
+      "desktop": "images/desktop/scene-11.jpg",
+      "mobile": "images/mobile/scene-11.jpg",
+      "thumbnail": "thumbnails/scene-11.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0716168338301033,
+          "pitch": 0.005294533734952367,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-aw-13-53-45",
+      "name": "Flur Obergeschoss",
+      "desktop": "images/desktop/scene-12.jpg",
+      "mobile": "images/mobile/scene-12.jpg",
+      "thumbnail": "thumbnails/scene-12.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.955799880824614,
+          "pitch": 0.1295285800648891,
+          "rotation": 0,
+          "target": "12-aw-13-54-44"
+        },
+        {
+          "yaw": 0.03502969164082259,
+          "pitch": 0.08130446204714126,
+          "rotation": 0,
+          "target": "6-aw-13-43-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-aw-13-54-44",
+      "name": "Gästezimmer",
+      "desktop": "images/desktop/scene-13.jpg",
+      "mobile": "images/mobile/scene-13.jpg",
+      "thumbnail": "thumbnails/scene-13.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0640826519527522,
+          "pitch": 0.0837053212706671,
+          "rotation": 0,
+          "target": "13-aw-13-55-59"
+        },
+        {
+          "yaw": 3.1225135166940747,
+          "pitch": 0.09557744001897461,
+          "rotation": 0,
+          "target": "11-aw-13-53-45"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-aw-13-55-59",
+      "name": "Balkon",
+      "desktop": "images/desktop/scene-14.jpg",
+      "mobile": "images/mobile/scene-14.jpg",
+      "thumbnail": "thumbnails/scene-14.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7580047933113754,
+          "pitch": 0.06251041882960173,
+          "rotation": 0,
+          "target": "7-aw-13-46-34"
+        },
+        {
+          "yaw": -2.9621711986886012,
+          "pitch": 0.14942697790517023,
+          "rotation": 0,
+          "target": "12-aw-13-54-44"
+        },
+        {
+          "yaw": 0.1770636755223336,
+          "pitch": 0.43719181356403247,
+          "rotation": 0,
+          "target": "16-aw-14-23-172"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-aw-14-00-30",
+      "name": "Wintergarten",
+      "desktop": "images/desktop/scene-15.jpg",
+      "mobile": "images/mobile/scene-15.jpg",
+      "thumbnail": "thumbnails/scene-15.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9806387257067453,
+          "pitch": 0.06353601120692076,
+          "rotation": 0,
+          "target": "1-aw-13-31-08"
+        },
+        {
+          "yaw": 2.0863997244655135,
+          "pitch": 0.13669190260901587,
+          "rotation": 0,
+          "target": "16-aw-14-23-172"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-aw-14-25-04",
+      "name": "Hauseingang",
+      "desktop": "images/desktop/scene-16.jpg",
+      "mobile": "images/mobile/scene-16.jpg",
+      "thumbnail": "thumbnails/scene-16.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08859103209285024,
+          "pitch": -0.08604989036918553,
+          "rotation": 0,
+          "target": "3-aw-13-40-53"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-aw-14-23-172",
+      "name": "Garten & Terrasse",
+      "desktop": "images/desktop/scene-17.jpg",
+      "mobile": "images/mobile/scene-17.jpg",
+      "thumbnail": "thumbnails/scene-17.jpg",
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8675081196011121,
+          "pitch": 0.05502133570395351,
+          "rotation": 0,
+          "target": "2-aw-13-32-34"
+        },
+        {
+          "yaw": -1.0237317479666999,
+          "pitch": 0.1267819520743494,
+          "rotation": 0,
+          "target": "14-aw-14-00-30"
+        },
+        {
+          "yaw": 0.4521431575587407,
+          "pitch": -0.9012227841420426,
+          "rotation": 0,
+          "target": "13-aw-13-55-59"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ]
+};
